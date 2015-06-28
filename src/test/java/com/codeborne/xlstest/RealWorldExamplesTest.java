76b22dfbc8220f8d5489d733bf3616dafce13367
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-06-28 16:49:04 +0300

    add acquiring report example

--- src/test/java/com/codeborne/xlstest/RealWorldExamplesTest.java
+++ src/test/java/com/codeborne/xlstest/RealWorldExamplesTest.java
@@ -13,4 +13,12 @@ public class RealWorldExamplesTest {
     assertThat(XLS, containsText("Период"));
     assertThat(XLS, containsText("18.06.2015 - 18.06.2015"));
   }
+
+  @Test
+  public void acquiringReport() throws Exception {
+    XLS XLS = new XLS(getClass().getClassLoader().getResource("acquiring.xls"));
+    assertThat(XLS, containsText("Отчёт об операциях по пластиковым картам - 27.06.2015"));
+    assertThat(XLS, containsText("с 27.05.2015 по 27.06.2015"));
+    assertThat(XLS, containsText("ООО \"Джапан Мотор Сервис\", ИНН 7830002292, Номер договора 08-1-1-04-2437"));
+  }
 }
