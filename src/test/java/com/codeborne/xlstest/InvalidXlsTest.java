dcff89eb6f68e5810d91ee2be8e15cbcbf89662d
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-10-29 00:26:08 +0300

    rename test-methods

--- src/test/java/com/codeborne/xlstest/InvalidXlsTest.java
+++ src/test/java/com/codeborne/xlstest/InvalidXlsTest.java
@@ -11,26 +11,26 @@ import static org.junit.Assert.fail;
 
 public class InvalidXlsTest {
   @Test
-  public void throwsX() {
+  public void throws_IllegalArgumentException_ifFailedToParseXlsFromBytes() {
     try {
       new XLS("This is not Excel file".getBytes());
       fail("expected IllegalArgumentException");
     }
-    catch (IllegalArgumentException e) {
-      assertThat(e.getMessage(), is("Invalid XLS "));
-      assertThat(e.getCause(), is(notNullValue()));
+    catch (IllegalArgumentException expected) {
+      assertThat(expected.getMessage(), is("Invalid XLS "));
+      assertThat(expected.getCause(), is(notNullValue()));
     }
   }
 
   @Test
-  public void throwsX2() {
+  public void throws_IllegalArgumentException_ifFailedToReadXlsFile() {
     try {
       new XLS(new File("missing-file.xls"));
       fail("expected IllegalArgumentException");
     }
-    catch (IllegalArgumentException e) {
-      assertThat(e.getMessage(), is("Failed to read file missing-file.xls"));
-      assertThat(e.getCause(), is(notNullValue()));
+    catch (IllegalArgumentException expected) {
+      assertThat(expected.getMessage(), is("Failed to read file missing-file.xls"));
+      assertThat(expected.getCause(), is(notNullValue()));
     }
   }
 }
